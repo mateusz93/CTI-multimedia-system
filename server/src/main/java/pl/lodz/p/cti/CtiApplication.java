63f1d973db24f1908f4f1f4c9e58b5c1220dc57f
Mateusz Wieczorek <wskmateusz@gmail.com>
2017-05-06 13:31:19 +0200

    Init server project

---
+++ server/src/main/java/pl/lodz/p/cti/CtiApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.cti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtiApplication.class, args);
+	}
+}
---
+++ server/src/test/java/pl/lodz/p/cti/CtiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.cti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CtiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
